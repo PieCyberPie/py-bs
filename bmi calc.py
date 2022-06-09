97c4898504188bd16ceba39cfa5ac5212b614be5
piecyberpie <fuG75Lue13*>
2022-06-09 18:30:10 +0300

    test1

--- bmi calc.py
+++ bmi calc.py
@@ -6,3 +6,10 @@ height_in_meters = height/100
 bmi = weight/height_in_meters**2
 
 print(f"ur bmi is {bmi}")
+
+if(bmi<16.0):
+    print("You are severely underweight! ")
+elif(bmi>16.0 and bmi<18.4):
+    print("You are underweight")
+elif(bmi>18.5 and bmi<24.9):
+    print("You are normal")
