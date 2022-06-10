1e91863cfbfe5993eec104c71d6f1d2dc2296e44
piecyberpie <fuG75Lue13*>
2022-06-10 14:41:15 +0300

    initial build

---
+++ dice rolling.py
@@ -0,0 +1,14 @@
+import random
+
+dice_num = int(input("How many dice are we rolling? "))
+sides_num = int(input("How many sides on each die? "))
+
+print(f"Number of dices: {dice_num} \nNumber of sides: {sides_num} \n")
+
+reroll = ""
+
+while reroll != 'q':
+    print("\nYour roll of dice is: ")
+    for i in range(0, dice_num):
+        print(f"{random.randint(1, sides_num)} | ", end='')
+    reroll = input("\nContinue? Press q to exit ")
