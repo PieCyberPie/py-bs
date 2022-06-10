e253e209e7d1f8668ed60cb679800e6b7321bf6d
piecyberpie <fuG75Lue13*>
2022-06-10 13:38:39 +0300

    initial build

---
+++ secondloopproblem.py
@@ -0,0 +1,9 @@
+target_input = "sillygoose"
+user_unput = ""
+
+user_unput = input("type in sillygoose: ")
+
+while user_unput != target_input:
+    user_unput = input("that's not sillygoose, try again! ")
+
+print("here you go, now that's some nice goose")
