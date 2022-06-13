ced5ac2fb120697c3a6b8ef301ef669c620a2210
piecyberpie <fuG75Lue13*>
2022-06-13 16:34:14 +0300

    Initial build, but not working

---
+++ args try.py
@@ -0,0 +1,23 @@
+#not working
+
+
+import random
+
+def average(*args):
+    print(args)
+    total = 0
+    for arg in args:
+        total += arg
+    return total/len(args)
+
+nums = []
+num_of_nums = 0
+
+num_of_nums = int(input("How much nums do you want? "))
+
+idx = 0 
+while idx < num_of_nums:
+    nums.append(random.randint(0, 100))
+    idx += 1
+
+average(nums)
